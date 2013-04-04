08643e44d700a7bae77c8ae22ae5db36496dd81d
Paul Cantalupo <pcantalupo@gmail.com>
2013-04-04 10:57:33 -0400

    script now gets information from STDIN

--- cgresult2tsv.pl
+++ cgresult2tsv.pl
@@ -3,11 +3,5 @@ use strict;
 use cghub;
 
 # parse cgquery text output into 1 row per result with fields separated by tabs
-my $file = shift;
-open (FILEIN, "<", $file) or die "Can't open $file: $!\n";
-
-my $slurpedfile = join ('', <FILEIN>);
-
-cgresult2tsv($slurpedfile);
-
+cgresult2tsv(join ('', <>));
 
