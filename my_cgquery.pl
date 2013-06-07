74879a770b87e1e3ac3a522872b15ff7074471fa
Paul Cantalupo <pcantalupo@gmail.com>
2013-06-07 09:27:10 -0400

    adding --study option to my_cgquery.pl

--- my_cgquery.pl
+++ my_cgquery.pl
@@ -5,10 +5,15 @@ use cghub;
 use Data::Dumper;
 use Getopt::Long;
 
-# example: my_cgquery.pl -d LIHC -l RNA-Seq    (gets all RNA-Seq results for LIHC)
-#          my_cgquery.pl -p 0bf5bbd4-d9e8-42a6-9ab5-f2c174dec12c   (get results for a particular person)
+# example: my_cgquery.pl -d LIHC -l RNA-Seq
+#               gets all RNA-Seq results for LIHC
+#          my_cgquery.pl -p 0bf5bbd4-d9e8-42a6-9ab5-f2c174dec12c
+#               get results for a particular person
+#          my_cgquery.pl -d KIRC -l RNA-Seq -x "PROGRAM>MapspliceRSEM" (or "PROGRAM>RNAseqAlignmentBWA" for other pipeline)
+#               get MapspliceRSEM pipeline results for RNA-Seq of KIRC 
 
 my ($pid, $aid, $did, $lid, $qid, $lastmod, $xmltext);
+my $study = "phs000178";             # default is the TCGA study
 my $outputxml = "";
 my $attr = 1;                 # get analysisAttribute (-a flag) not analysisObject information
 GetOptions ("participant|p=s" => \$pid,
@@ -20,12 +25,14 @@ GetOptions ("participant|p=s" => \$pid,
             "outputxml|o=s"   => \$outputxml,
             "attr|r"          => \$attr,              # specify -r on commandline if you don't want analysisAttributes
             "lastmod|m=s"     => \$lastmod,
+            "study|s=s"       => \$study
             );
 
 exec("echo specify -p, -a, -d, -l, -q") unless ($pid || $aid || $did || $lid || $qid || $xmltext);
 
 my @parts;
 push (@parts, "state=live");
+push (@parts, "study=phs000178")       if $study;
 push (@parts, "participant_id=$pid")   if $pid;
 push (@parts, "analysis_id=$aid")      if $aid;
 push (@parts, "disease_abbr=$did")     if $did;
