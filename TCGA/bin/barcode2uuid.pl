95cc549a7853a5e830d7678d2fc0702f3544022d
Paul Cantalupo <pcantalupo@gmail.com>
2015-07-22 15:17:42 -0400

    add error catching

--- TCGA/bin/barcode2uuid.pl
+++ TCGA/bin/barcode2uuid.pl
@@ -6,11 +6,17 @@ use LWP::Simple;
 my $base = "https://tcga-data.nci.nih.gov/uuid/uuidws/mapping/json/barcode/";
 
 while (<>) {
+  chomp;
   my $url = $base . $_;
-  #getprint $url;
   my $content = get($url);
   $content =~ /\"uuid\":\"(\S+)\"}}/; 
-  print $1,"\n";
+  if (!defined $1) {
+    print "\n";
+    print STDERR "$_ is not valid barcode\n";
+  }
+  else {
+    print $1,"\n";
+  }
 }
 
 #"uuidMapping":{"barcode":"TCGA-CA-6716-01A-11R-1839-07","uuid":"de926d40-12cd-4aba-bb2e-10d2f7768f49"}}
